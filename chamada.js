4353c01f23911d1f045243e9bbfedfcc432b17ac
gustmacena <102830712+gustmacena@users.noreply.github.com>
2022-10-14 20:44:53 -0300

    Chamada

---
+++ chamada.js
@@ -0,0 +1,14 @@
+let x = "Macena"
+let cont = 0;
+
+while(cont <= x.length -1){
+    cont ++
+    console.log(cont +"-"+x)
+}
+
+
+let nome = "Macena"
+for(let i = 1; i <= 10; i++){
+    console.log(i + "-" + nome)
+}
+
